13657711422ed5788ef7283f4242d2605d1987d7
Wiku Rahadian <wikurahadian98@gmail.com>
2019-02-03 21:19:46 +0700

    Membuat tugas1 bagian buku alamat

---
+++ Tugas1/src/bukualamat/source/Base.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bukualamat.source;
+
+/**
+ *
+ * @author dela
+ */
+public class Base {
+    private String nama;
+    private String alamat;
+    private String telepon;
+    private String email;
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getTelepon() {
+        return telepon;
+    }
+
+    public void setTelepon(String telepon) {
+        this.telepon = telepon;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
